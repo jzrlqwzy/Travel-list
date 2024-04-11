eecb07c1766acfc759e3c458f1cb9010e1557cee
lei <littlesomerest@gmail.com>
2024-04-11 08:23:36 +0800

    4 components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
-export default function App() {}
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
 
-function Logo() {}
+export default function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
 
-function Form() {}
+function Logo() {
+  return <h1>🌊 Far Away 💼</h1>;
+}
 
-function PackingList() {}
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need 😍 for your trip?</h3>
+    </div>
+  );
+}
 
-function Stats() {}
+function PackingList() {
+  return <ul className="list">List</ul>;
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>💼 You have X items on your list, and you already packed X (X%)</em>
+    </footer>
+  );
+}
